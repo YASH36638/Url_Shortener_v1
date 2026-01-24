cbcc5fb1d5dbb541b47d2e1c0d8195a6412cd9f1
IGNITE-07 <jainym@rknec.edu>
2026-01-24 16:56:22 +0530

    Remove mongodb and config files from tracking

--- config/db-client.js
+++
@@ -1,5 +0,0 @@
-// import { MongoClient } from "mongodb";
-// import { env } from "./env.js";
-
-// export const dbclient=new MongoClient(env.MONGODB_URI)
-
--- config/env.js
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod';
-import 'dotenv/config';
-
-export const env=z.object({
-    PORT:z.coerce.number().default(3000),
-    MONGODB_URI:z.string(),
-    MONGODB_DATABASE_NAME:z.string(),
-}).parse(process.env);
