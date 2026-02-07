8ace1a361c0405bd3ef81fcdf79d02ee8ec33e59
IGNITE-07 <jainym@rknec.edu>
2026-02-08 02:06:05 +0530

    fixed env schema

--- config/env.js
+++ config/env.js
@@ -7,6 +7,8 @@ GOOGLE_CLIENT_ID: z.string().min(1),
 GITHUB_CLIENT_SECRET: z.string().min(1),
 GITHUB_CLIENT_ID: z.string().min(1),
 FRONTEND_URL:z.string().url().trim().min(1),
+GOOGLE_callbackURL:z.string().url().trim().min(1),
+GITHUB_callbackURL:z.string().url().trim().min(1)
 });
 
 export const env = envSchema.parse(process.env);
