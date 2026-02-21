5186a3bb42d8cb31074e453de8dced94c6146a82
IGNITE-07 <jainym@rknec.edu>
2026-02-21 20:02:46 +0530

    fixed icon visibility issues

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 
 app.set("view engine","ejs");
 app.use(cookieParser());
